f1dceff11c157c6719ad5442ecb049c03fe728a2
Greg <117015797+malo0n@users.noreply.github.com>
2024-04-15 15:22:10 +0300

    Delete frontend_Greg directory

--- frontend_Greg/src/declaration.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "*.css" {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-declare module "*.jpg";
-declare module "*.png";
-declare module "*.svg";
-declare module "*.xml";
--- frontend_Greg/src/helpers/data/skillsData.ts
+++
@@ -1,6 +0,0 @@
-import { ProfileData } from "@/types/types";
-import { profileData } from "./profileData";
-
-export function skillsData(data: ProfileData): string {
-  return data.skills || "";
-}
