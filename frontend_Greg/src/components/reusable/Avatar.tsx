48aeef45e48aa327e5e9ce4969ec6f1a7ddadf56
Greg <117015797+malo0n@users.noreply.github.com>
2024-04-15 15:14:59 +0300

    Delete frontend_Greg directory

--- frontend_Greg/src/components/reusable/Avatar.tsx
+++
@@ -1,8 +0,0 @@
-import avatarDefault from "../../assets/icons/defaultAvatar.svg";
-import { AvatarProps } from "../../types/types";
-export function Avatar({ photo }: AvatarProps) {
-  const src: string = photo ? photo : avatarDefault;
-  return (
-    <img className=" size-[72px] xs:size-[152px]" src={src} alt="avatar" />
-  );
-}
